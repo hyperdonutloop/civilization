7900449f817ef0a88bec852a60fafc45ff748b8a
donutwizard666 <thebeardedwarrior@icloud.com>
2019-12-05 13:33:34 -0800

    post and get request are working

--- units/controller.js
+++ units/controller.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
+let units = [
+  {
+    id: 1,
+    name: 'Warrior',
+    type: 'melee',
+    attack: 10,
+    defense: 20,
+  }
+];
 
+let nextUnitId = 2;
+
+router.get('/', (req, res) => {
+  res.status(200).json(units);
 })
 
 router.get('/:id', (req, res) => {
@@ -9,7 +21,11 @@ router.get('/:id', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-  
+  unitInformation = {id: nextUnitId++, ...req.body };
+
+  units = [ ...units, unitInformation]
+
+  res.status(201).json(unitInformation)
 })
 
 router.put('/:id', (req, res) => {
@@ -19,6 +35,5 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
   
 })
-
 
 module.exports = router;
